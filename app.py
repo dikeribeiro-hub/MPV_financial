b233ac5b6dabb32d886fbfc5280b530e39818d3a
dikeribeiro-hub <dike.ribeiro@gmail.com>
2026-05-04 09:05:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Arbi Finance AI", layout="wide")
+st.set_page_config(page_title="Finance AI", layout="wide")
 
 # ------------------------
 # Dados fictícios
